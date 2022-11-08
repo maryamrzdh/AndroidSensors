d537a681a7ddd748b743d09379762be799fc0f02
m.memarzadeh <m.memarzadeh@hoshmand.local>
2022-11-08 15:29:25 +0330

    add speedometer features!

--- app/src/main/java/com/example/hw2/BaseFragment.kt
+++ app/src/main/java/com/example/hw2/BaseFragment.kt
@@ -2,26 +2,33 @@ package com.example.hw2
 
 import android.hardware.SensorManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var sensorManager: SensorManager
+
+    private var savedInstanceState: Bundle? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        this.savedInstanceState = savedInstanceState
+        super.onCreate(savedInstanceState)
+
+        sensorManager = requireActivity().getSystemService(AppCompatActivity.SENSOR_SERVICE) as SensorManager
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_base, container, false)
-
-        sensorManager = requireActivity().getSystemService(AppCompatActivity.SENSOR_SERVICE) as SensorManager
-
-        return view
+        return inflater.inflate(R.layout.fragment_base, container, false)
     }
 
 }
--- app/src/main/java/com/example/hw2/MainActivity.kt
+++ app/src/main/java/com/example/hw2/MainActivity.kt
@@ -1,13 +1,58 @@
 package com.example.hw2
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val permissionRequest = 0
+//    private var storagePermitted = true
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // ask for permissions if haven't got one
+        if (ContextCompat.checkSelfPermission(
+                this, arrayOf(
+                    Manifest.permission.ACTIVITY_RECOGNITION,
+                ).toString()
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACTIVITY_RECOGNITION,
+                ), permissionRequest
+            )
+        }
     }
+
+    // if the permission is not granted
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            permissionRequest -> {
+//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED){
+//
+//                }else{
+//
+//                }
+
+            }
+        }
+        return
+    }
+
 }
